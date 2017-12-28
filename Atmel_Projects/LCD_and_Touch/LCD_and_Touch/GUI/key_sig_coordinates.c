af2c246efed2d225c3daf15c65ab25e7e8695729
Tyler Tucker <tylermtucker@gmail.com>
2017-12-28 02:41:56 -0500

    Add files via upload

---
+++ Atmel_Projects/LCD_and_Touch/LCD_and_Touch/GUI/key_sig_coordinates.c
@@ -0,0 +1,45 @@
+typedef struct key_sig_coord
+{
+	int16_t x;
+	int16_t y;
+} key_sig_coord_t;
+
+static key_sig_coord_t key_sig_coordinates[] = {
+	// First Row
+	{58, 66},  // C Major
+	{149, 66}, // C minor
+	{240, 66}, // C# Major
+	{331, 66}, // C# minor
+	{422, 66}, // Db minor
+  // Second Row
+  {58, 111},  // D Major
+  {149,111},  // D minor
+  {240, 111}, // D# minor
+  {331, 111}, // Eb Major
+  {422, 111}, // Eb minor
+	// Third Row
+  {58, 156},  // E Major
+	{149, 156}, // E minor
+	{240, 156}, // F Major
+	{331, 156}, // F minor
+	{422, 156}, // F# Major
+	// Fourth Row
+  {58, 201},  // F# minor
+	{149, 201}, // Gb Major
+	{240, 201}, // G Major
+	{331, 201}, // G minor
+	{422, 201}, // G# minor
+	// Fifth Row
+  {58, 246},  // Ab Major
+	{149, 246}, // Ab minor
+	{240, 246}, // A Major
+	{331, 246}, // A minor
+	{422, 246}, // A# minor
+	//Sixth Row
+  {58, 291},  // Bb Major
+	{149, 291}, // Bb minor
+	{240, 291}, // B Major
+	{331, 291}, // B minor
+	{422, 291}, // Cb Major
+
+};
