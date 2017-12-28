f982b1091e4885e31e2675c9281d35cebaa3b79b
Daniel Gonzalez <Danngonz3@ufl.edu>
2017-12-28 03:31:25 -0500

    imported key signature menu

--- Atmel_Projects/LCD_and_Touch/LCD_and_Touch/GUI/key_signature/key_signature.c
+++ Atmel_Projects/LCD_and_Touch/LCD_and_Touch/GUI/key_signature/key_signature.c
@@ -21,11 +21,42 @@ typedef struct key_signature_coord
 
 static key_signature_coord_t key_signature_coordinates[] =
 {
-	// TODO 
-	{146, 128}, 
-	{345, 128}, 
-	{146, 222}, 
-	{345, 222}	
+	// First Row
+	{58, 66},  // C Major
+	{149, 66}, // C minor
+	{240, 66}, // C# Major
+	{331, 66}, // C# minor
+	{422, 66}, // Db minor
+	// Second Row
+	{58, 111},  // D Major
+	{149,111},  // D minor
+	{240, 111}, // D# minor
+	{331, 111}, // Eb Major
+	{422, 111}, // Eb minor
+	// Third Row
+	{58, 156},  // E Major
+	{149, 156}, // E minor
+	{240, 156}, // F Major
+	{331, 156}, // F minor
+	{422, 156}, // F# Major
+	// Fourth Row
+	{58, 201},  // F# minor
+	{149, 201}, // Gb Major
+	{240, 201}, // G Major
+	{331, 201}, // G minor
+	{422, 201}, // G# minor
+	// Fifth Row
+	{58, 246},  // Ab Major
+	{149, 246}, // Ab minor
+	{240, 246}, // A Major
+	{331, 246}, // A minor
+	{422, 246}, // A# minor
+	//Sixth Row
+	{58, 291},  // Bb Major
+	{149, 291}, // Bb minor
+	{240, 291}, // B Major
+	{331, 291}, // B minor
+	{422, 291}, // Cb Major
 };
 
 static key_signature_t key_signatures[] =
@@ -84,7 +115,6 @@ static key_signature_t get_button_pressed(int16_t x, int16_t y)
 	return keySignaturePressed;
 }
 
-
 void key_signature_menu(key_signature_t *key_sig)
 {
 	touch_t touched_point;
@@ -96,5 +126,4 @@ void key_signature_menu(key_signature_t *key_sig)
 	touch_handler(&touched_point);
 	
 	*key_sig = get_button_pressed(touched_point.x, touched_point.y);
-	
 }
