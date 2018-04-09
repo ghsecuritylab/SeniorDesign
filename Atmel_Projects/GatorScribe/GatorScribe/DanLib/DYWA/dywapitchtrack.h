4a613f4d89b77711afd2b0fe6933af305431b59f
Daniel Gonzalez <Danngonz3@ufl.edu>
2018-04-09 18:17:16 -0400

    imported pitch detect DYWA

---
+++ Atmel_Projects/GatorScribe/GatorScribe/DanLib/DYWA/dywapitchtrack.h
@@ -0,0 +1,21 @@
+#ifndef dywapitchtrack__H
+#define dywapitchtrack__H
+
+#include "asf.h"
+#include "DMA_Audio.h"
+
+/*#define DYW_SAMPLING_RATE 23250*/
+#define DYW_SAMPLING_RATE 46503.0f 
+
+// structure to hold tracking data
+typedef struct _dywapitchtracker {
+	float	_prevPitch;
+	int		_pitchConfidence;
+} dywapitchtracker;
+
+
+// return -1.0 if no pitch was found (sound too low, noise, etc..)
+float computeWaveletPitch(float * samples); 
+
+#endif
+
